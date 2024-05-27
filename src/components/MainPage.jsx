e1cb966b5dbddb224f2c0a479d4181fb45604d05
User <sefereliaysu2@gmail.com>
2024-05-27 19:03:19 +0400

    iconKaraokeLounge

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,10 +7,14 @@ import {
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
 
 export default function MainPage({ switchTheme, theme }) {
   const [isModalOpen, setModalOpen] = useState(false);
   const [isModal2Open, setModal2Open] = useState(false);
+  const [isModal3Open, setModal3Open] = useState(false);
+  const [phoneValue, setPhoneValue] = useState("");
 
   const openModal = () => {
     setModalOpen(true);
@@ -28,6 +32,14 @@ export default function MainPage({ switchTheme, theme }) {
     setModal2Open(false);
   };
 
+  const open3Modal = () => {
+    setModal3Open(true);
+  };
+
+  const close3Modal = () => {
+    setModal3Open(false);
+  };
+
   return (
     <div className="mainPage" data-theme={theme}>
       <div>
@@ -38,10 +50,27 @@ export default function MainPage({ switchTheme, theme }) {
                 AZ
               </button>
               <button className="mode" id="moon" onClick={switchTheme}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#000"
+                >
+                  <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+                </svg>
               </button>
               <button className="mode" id="sun" onClick={switchTheme}>
-              <svg className="sun-svg" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>
+                <svg
+                  className="sun-svg"
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#e8eaed"
+                >
+                  <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+                </svg>
               </button>
             </div>
             <button className="user" onClick={open2Modal}>
@@ -74,7 +103,7 @@ export default function MainPage({ switchTheme, theme }) {
         </div>
         <hr />
         <div>
-          <button className="connect-link">
+          <button className="connect-link" onClick={open3Modal}>
             <FontAwesomeIcon className="faPhone" icon={faPhone} />
             <p> Əlaqə</p>
           </button>
@@ -92,15 +121,15 @@ export default function MainPage({ switchTheme, theme }) {
       {isModalOpen && (
         <div id="myModal" className="modal">
           <div className="modal-content">
-            <div className="l flex justify-center z-10 vaul-scrollable">
-              <div className="rounded-top"></div>
-            </div>
             <span className="close" onClick={closeModal}>
               &times;
             </span>
+            <div className="vaul-scrollable">
+              <div className="rounded-top"></div>
+            </div>
             <div className="modal_info">
               <div className="choice">Dil seçimi</div>
-              <div>
+              <div className="country">
                 <button className="flag">
                   <img
                     className="flagIcon"
@@ -114,17 +143,62 @@ export default function MainPage({ switchTheme, theme }) {
           </div>
         </div>
       )}
+
       {isModal2Open && (
         <div id="myModal" className="modal2">
           <div className="modal-content2">
-            <span className="close" onClick={close2Modal}>
+            <span className="close2" onClick={close2Modal}>
               &times;
             </span>
+            <div className="modal_info2">
+              <div className="choice2">Giriş et</div>
+              <PhoneInput
+                className="phoneInput"
+                international
+                CountryCallingCodeEditable={false}
+                defaultCountry="AZ"
+                value={phoneValue}
+                onChange={setPhoneValue}
+              />
+              <p className="phoneNumber">
+                Telefon nömrənizi doğrulamaq üçün kod göndərəcəyik
+              </p>
+              <div className="continue">
+                <button className="continueBtn">Davam et</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {isModal3Open && (
+        <div id="myModal" className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={close3Modal}>
+              &times;
+            </span>
+            <div className="vaul-scrollable">
+              <div className="rounded-top"></div>
+            </div>
             <div className="modal_info">
-              <div className="choice">Giriş et</div>
-              <p>Telefon nömrənizi doğrulamaq üçün kod göndərəcəyik</p>
-              <div>
-                <button className="flag">Davam et</button>
+              <div className="choice">Əlaqə</div>
+              <div className="country">
+                <a
+                  href="tel:(055)%20353%2022%2043"
+                  style={{ textDecoration: 1 }}
+                >
+                  <button className="flag">
+                    <FontAwesomeIcon className="faPhone" icon={faPhone} />
+                    <span className="language">(055) 353 22 43</span>
+                  </button>
+                </a>
+                <a className="wp" href="https://web.whatsapp.com/">
+                  <img
+                    className="wpIcon"
+                    src="assets/whatsapp.png"
+                    alt="whatsapp"
+                  />
+                </a>
               </div>
             </div>
           </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,79 +12,130 @@ export default function Menu() {
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    console.log(newTheme)}
+    console.log(newTheme);
+  };
   return (
-    <div className="main-container"  data-theme={theme}>
-    <div className="border-container">
-      <div className="menu-header">
-        <div className="arrow_moon">
-          <Link to="/">
+    <div className="main-container" data-theme={theme}>
+      <div className="border-container">
+        <div className="menu-header">
+          <div className="arrow_moon">
+            <Link to="/">
+              <svg
+                className="back-arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000"
+              >
+                <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+              </svg>
+            </Link>
+            <button className="menu-mode" id="moon" onClick={switchTheme}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="28px"
+                viewBox="0 -960 960 960"
+                width="28px"
+                fill="#000"
+              >
+                <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+              </svg>
+            </button>
+            <button className="menu-mode" id="sun" onClick={switchTheme}>
+              <svg
+                className="sun-svg"
+                xmlns="http://www.w3.org/2000/svg"
+                height="28px"
+                viewBox="0 -960 960 960"
+                width="28px"
+                fill="#fff"
+              >
+                <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+              </svg>
+            </button>
+          </div>
+          <div className="filter_search">
             <svg
-              className="back-arrow"
+              className="filter-icon"
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
               viewBox="0 -960 960 960"
               width="24px"
               fill="#000"
             >
-              <path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" />
+              <path d="M710-150q-63 0-106.5-43.5T560-300q0-63 43.5-106.5T710-450q63 0 106.5 43.5T860-300q0 63-43.5 106.5T710-150Zm0-80q29 0 49.5-20.5T780-300q0-29-20.5-49.5T710-370q-29 0-49.5 20.5T640-300q0 29 20.5 49.5T710-230Zm-550-30v-80h320v80H160Zm90-250q-63 0-106.5-43.5T100-660q0-63 43.5-106.5T250-810q63 0 106.5 43.5T400-660q0 63-43.5 106.5T250-510Zm0-80q29 0 49.5-20.5T320-660q0-29-20.5-49.5T250-730q-29 0-49.5 20.5T180-660q0 29 20.5 49.5T250-590Zm230-30v-80h320v80H480Zm230 320ZM250-660Z" />
             </svg>
-          </Link>
-          <button className="menu-mode" id="moon" onClick={switchTheme}>
             <svg
+              className="serach-icon"
               xmlns="http://www.w3.org/2000/svg"
-              height="28px"
+              height="24px"
               viewBox="0 -960 960 960"
-              width="28px"
+              width="24px"
               fill="#000"
             >
-              <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
             </svg>
-          </button>
-          <button className="menu-mode" id="sun" onClick={switchTheme}>
-            <svg
-              className="sun-svg"
-              xmlns="http://www.w3.org/2000/svg"
-              height="28px"
-              viewBox="0 -960 960 960"
-              width="28px"
-              fill="#fff"
-            >
-              <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
-            </svg>
-          </button>
+          </div>
         </div>
-        <div className="filter_search">
-          <svg  className="filter-icon"
+        <div className="mealsOptions">
+          <ul className="meals">
+            <button className="meal">Suplar</button>
+            <button className="meal">Salatlar</button>
+            <button className="meal">Soyuq qəlyanaltılar</button>
+            <button className="meal">İsti qəlyanaltılar</button>
+            <button className="meal">Sushi</button>
+            <button className="meal">Pizzalar</button>
+            <button className="meal">Sendviç və burgerlər</button>
+            <button className="meal">Pastalar</button>
+            <button className="meal">İsti Yeməklər</button>
+            <button className="meal">Qarnirlər</button>
+            <button className="meal">Saclar</button>
+            <button className="meal">Kabablar</button>
+            <button className="meal">Steak</button>
+            <button className="meal">Çərəzlər</button>
+            <button className="meal">Şirniyyatlar</button>
+            <button className="meal">Spirtsiz içkilər</button>
+            <button className="meal">Qəlyanlar</button>
+            <button className="meal">İsti içkilər</button>
+            <button className="meal">Təzə Sıxılmış Şirələr</button>
+            <button className="meal">Pivə</button>
+            <button className="meal">Alkoqolsuz Kokteyllər</button>
+            <button className="meal">Ginlər</button>
+            <button className="meal">Romlar</button>
+            <button className="meal">Viskilər</button>
+            <button className="meal">Viski Butulka</button>
+            <button className="meal">Araqlar</button>
+            <button className="meal">Araq Butulka</button>
+            <button className="meal">Tequila</button>
+            <button className="meal">Tequila Butulka</button>
+            <button className="meal">Konyak</button>
+            <button className="meal">Likor,Bitterlər,Vermut</button>
+            <button className="meal">Şərablar</button>
+            <button className="meal">Şampan</button>
+            <button className="meal">Alkoqollu Kokteyllər</button>
+            <button className="meal">Məzələr</button>
+            <button className="meal">Rakı</button>
+            <button className="meal">Shot</button>
+          </ul>
+        </div>
+        <div className="menu-note">
+          <svg
+            className="menu-note-icon"
             xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#000"
+            width="24"
+            height="24"
+            fill="none"
           >
-            <path d="M710-150q-63 0-106.5-43.5T560-300q0-63 43.5-106.5T710-450q63 0 106.5 43.5T860-300q0 63-43.5 106.5T710-150Zm0-80q29 0 49.5-20.5T780-300q0-29-20.5-49.5T710-370q-29 0-49.5 20.5T640-300q0 29 20.5 49.5T710-230Zm-550-30v-80h320v80H160Zm90-250q-63 0-106.5-43.5T100-660q0-63 43.5-106.5T250-810q63 0 106.5 43.5T400-660q0 63-43.5 106.5T250-510Zm0-80q29 0 49.5-20.5T320-660q0-29-20.5-49.5T250-730q-29 0-49.5 20.5T180-660q0 29 20.5 49.5T250-590Zm230-30v-80h320v80H480Zm230 320ZM250-660Z" />
+            <path
+              fill="#334155"
+              d="M11.999 2.014c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10m0 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2m-1 3h1a.986.986 0 0 1 .969 1.188l-.75 3.812h.781a1 1 0 0 1 0 2h-1c-1.183 0-2.013-1.027-1.781-2.188l.593-2.874c-.453-.096-.812-.456-.812-.938a1 1 0 0 1 1-1"
+            />
           </svg>
-          <svg className="serach-icon"
-            xmlns="http://www.w3.org/2000/svg"
-            height="24px"
-            viewBox="0 -960 960 960"
-            width="24px"
-            fill="#000"
-          >
-            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-          </svg>
+          <span>Hesaba 10% servis haqqı əlavə olunur</span>
         </div>
       </div>
-      <div className="menu-note" >
-        <svg className="menu-note-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
-          <path
-            fill="#334155"
-            d="M11.999 2.014c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10-4.477-10-10-10m0 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2m-1 3h1a.986.986 0 0 1 .969 1.188l-.75 3.812h.781a1 1 0 0 1 0 2h-1c-1.183 0-2.013-1.027-1.781-2.188l.593-2.874c-.453-.096-.812-.456-.812-.938a1 1 0 0 1 1-1"
-          />
-        </svg>
-        <span>Hesaba 10% servis haqqı əlavə olunur</span>
-      </div>
-    </div>
+      <div className="productsList"></div>
     </div>
   );
 }
