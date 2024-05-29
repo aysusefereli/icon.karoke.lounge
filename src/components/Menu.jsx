1cc8d3a36356d63faee7015dbd7893c8f76f0663
User <sefereliaysu2@gmail.com>
2024-05-29 13:35:59 +0400

    iconKaraokeLounge

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -16,7 +16,6 @@ import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
-
 export default function Menu() {
   const [filteredItems, setFilteredItems] = useState([]);
   const [input, setInput] = useState("");
@@ -56,108 +55,108 @@ export default function Menu() {
   const closeSearchModal = () => {
     setModalSearchOpen(false);
   };
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value.toLowerCase();
+    setInput(inputValue);
 
+    const filteredCategories = categories
+      .map((category) => {
+        const filteredItems = category.items.filter((item) => {
+          return item.name.toLowerCase().includes(inputValue);
+        });
 
-  useEffect(() => {
-    if (input.trim() !== "") {
-      const firstLetter = input.trim().split(" ")[0].toLowerCase();
-      const filtered = categories.filter((category) => {
-        return (
-          input &&
-          category &&
-          category.name &&
-          category.name.toLowerCase().includes(input.toLowerCase())
-        );
-      });
-      setFilteredItems(filtered);
-    } else {
-      setFilteredItems(categories);
-    }
-  }, [input, categories]);
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
+        if (filteredItems.length > 0) {
+          return {
+            ...category,
+            items: filteredItems,
+          };
+        } else {
+          return null;
+        }
+      })
+      .filter(Boolean);
+    setFilteredItems(filteredCategories);
   };
 
   return (
     <div className="main-container" data-theme={theme}>
       <div className="container">
         <div className="border-container">
-        <div className="header-border">
-          <div className="menu-header">
-            <div className="arrow_moon">
-              <Link to="/" className="back">
-                <FontAwesomeIcon className="back-arrow" icon={faArrowLeft} />
-              </Link>
-              <button className="menu-mode" id="moon" onClick={switchTheme}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="28px"
-                  viewBox="0 -960 960 960"
-                  width="28px"
-                  fill="#000"
-                >
-                  <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
-                </svg>
-              </button>
-              <button className="menu-mode" id="sun" onClick={switchTheme}>
-                <svg
-                  className="sun-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="28px"
-                  viewBox="0 -960 960 960"
-                  width="28px"
-                  fill="#fff"
-                >
-                  <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
-                </svg>
-              </button>
+          <div className="header-border">
+            <div className="menu-header">
+              <div className="arrow_moon">
+                <Link to="/" className="back">
+                  <FontAwesomeIcon className="back-arrow" icon={faArrowLeft} />
+                </Link>
+                <button className="menu-mode" id="moon" onClick={switchTheme}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="28px"
+                    viewBox="0 -960 960 960"
+                    width="28px"
+                    fill="#000"
+                  >
+                    <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+                  </svg>
+                </button>
+                <button className="menu-mode" id="sun" onClick={switchTheme}>
+                  <svg
+                    className="sun-svg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="28px"
+                    viewBox="0 -960 960 960"
+                    width="28px"
+                    fill="#fff"
+                  >
+                    <path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z" />
+                  </svg>
+                </button>
+              </div>
+              <div className="filter_search">
+                <button className="filter" onClick={open4Modal}>
+                  <svg
+                    className="filterIcon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#000"
+                  >
+                    <path d="M710-150q-63 0-106.5-43.5T560-300q0-63 43.5-106.5T710-450q63 0 106.5 43.5T860-300q0 63-43.5 106.5T710-150Zm0-80q29 0 49.5-20.5T780-300q0-29-20.5-49.5T710-370q-29 0-49.5 20.5T640-300q0 29 20.5 49.5T710-230Zm-550-30v-80h320v80H160Zm90-250q-63 0-106.5-43.5T100-660q0-63 43.5-106.5T250-810q63 0 106.5 43.5T400-660q0 63-43.5 106.5T250-510Zm0-80q29 0 49.5-20.5T320-660q0-29-20.5-49.5T250-730q-29 0-49.5 20.5T180-660q0 29 20.5 49.5T250-590Zm230-30v-80h320v80H480Zm230 320ZM250-660Z" />
+                  </svg>
+                </button>
+                <button className="search" onClick={openSearchModal}>
+                  <FontAwesomeIcon
+                    className="searchIcon"
+                    icon={faMagnifyingGlass}
+                  />
+                </button>
+              </div>
             </div>
-            <div className="filter_search">
-              <button className="filter" onClick={open4Modal}>
-                <svg
-                  className="filterIcon"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="#000"
-                >
-                  <path d="M710-150q-63 0-106.5-43.5T560-300q0-63 43.5-106.5T710-450q63 0 106.5 43.5T860-300q0 63-43.5 106.5T710-150Zm0-80q29 0 49.5-20.5T780-300q0-29-20.5-49.5T710-370q-29 0-49.5 20.5T640-300q0 29 20.5 49.5T710-230Zm-550-30v-80h320v80H160Zm90-250q-63 0-106.5-43.5T100-660q0-63 43.5-106.5T250-810q63 0 106.5 43.5T400-660q0 63-43.5 106.5T250-510Zm0-80q29 0 49.5-20.5T320-660q0-29-20.5-49.5T250-730q-29 0-49.5 20.5T180-660q0 29 20.5 49.5T250-590Zm230-30v-80h320v80H480Zm230 320ZM250-660Z" />
-                </svg>
-              </button>
-              <button className="search" onClick={openSearchModal}>
-                <FontAwesomeIcon
-                  className="searchIcon"
-                  icon={faMagnifyingGlass}
-                />
-              </button>
-            </div>
-          </div>
-          <div className="mealsOptions">
-            <Swiper
-              slidesPerView={"auto"}
-              spaceBetween={10}
-              freeMode={true}
-              keyboard={{
-                enabled: true,
-                onlyInViewport: false,
-              }}
-            mousewheel={{
-              forceToAxis: true, 
-            }}
-            simulateTouch={true} 
-            touchReleaseOnEdges={true} 
-              modules={[FreeMode,Keyboard,Mousewheel]}
-              className="mySwiper"
-            >
-              {categories.map((category) => (
+            <div className="mealsOptions">
+              <Swiper
+                slidesPerView={"auto"}
+                spaceBetween={10}
+                freeMode={true}
+                keyboard={{
+                  enabled: true,
+                  onlyInViewport: false,
+                }}
+                mousewheel={{
+                  forceToAxis: true,
+                }}
+                simulateTouch={true}
+                touchReleaseOnEdges={true}
+                modules={[FreeMode, Keyboard, Mousewheel]}
+                className="mySwiper"
+              >
+                {categories.map((category) => (
                   <SwiperSlide key={category.id} className="swiper-slide-auto">
-                  <button className="meal">{category.name}</button>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+                    <button className="meal">{category.name}</button>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
           </div>
           <div className="menu-note">
             <svg
@@ -213,7 +212,7 @@ export default function Menu() {
                     filteredItems.map((category) => (
                       <div key={category.id}>
                         {category.items.map((item) => (
-                          <div key={item.id}>
+                          <div key={item.id} className="prices">
                             <div className="namePrice">
                               <span className="foodName">{item.name}</span>
                               <span className="price">{item.price} ₼</span>
