5f540906dff3cf04d8d40f374c08eae6f1ca1561
MiranaMiriyeva <arzumiri20@gmail.com>
2024-06-01 14:54:37 +0400

    icon-k-l

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -26,6 +26,7 @@ import Slider from "@mui/material/Slider";
 import { useThemeManager } from "./theme";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../store/slices/counterSlice";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 export default function Menu() {
   const [filteredItems, setFilteredItems] = useState([]);
@@ -510,6 +511,7 @@ export default function Menu() {
             <div className="modal_info">
               <div className="choice">Səbətdəki məhsullar</div>
               <div className="counter_basket">
+                
                 <div className="counter">
                   <button
                     onClick={() => dispatch(decrement())}
@@ -545,7 +547,8 @@ export default function Menu() {
                     <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                   </svg>
                 </button>
-                <button>
+                <button className="wp-order">
+                <FontAwesomeIcon className="faWhatsapp" icon={faWhatsapp} />
                   <span>WhatsApp sifarişi</span>
                 </button>
               </div>
